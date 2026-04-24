acc08f1a308ac69a7f7fed327e3baec33f0f648a
Maksim Demidov <maaksaa@gmail.com>
2026-04-24 18:09:53 +0200

    feat: add react query client provider with devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { queryClient } from '@/lib/queryClient'
 import PersonalDataPage from '@/pages/PersonalDataPage'
 import AddressPage from '@/pages/AddressPage'
 import LoanPage from '@/pages/LoanPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/personal" replace />} />
-        <Route path="/personal" element={<PersonalDataPage />} />
-        <Route path="/address" element={<AddressPage />} />
-        <Route path="/loan" element={<LoanPage />} />
-        <Route path="*" element={<Navigate to="/personal" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/personal" replace />} />
+          <Route path="/personal" element={<PersonalDataPage />} />
+          <Route path="/address" element={<AddressPage />} />
+          <Route path="/loan" element={<LoanPage />} />
+          <Route path="*" element={<Navigate to="/personal" replace />} />
+        </Routes>
+      </BrowserRouter>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   )
 }
 
